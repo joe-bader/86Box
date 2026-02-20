4f95caf46c26831ea60672f385993e4a5828e300
Joe Bader <696512+joe-bader@users.noreply.github.com>
2026-02-20 13:06:12 -0500

    Refactor Rendition Verite V1000 emulation by removing unused code and simplifying PCI read/write handling

--- src/video/vid_rendition_verite.c
+++ src/video/vid_rendition_verite.c
@@ -1,17 +1,3 @@
-/*
- * 86Box    A hypervisor and IBM PC system emulator that specializes in
- *          running old operating systems and software designed for IBM
- *          PC systems and compatibles from 1981 through fairly recent
- *          system designs based on the PCI bus.
- *
- *          This file is part of the 86Box distribution.
- *
- *          Rendition Verite V1000 (Sierra Screamin' 3D) video card emulation.
- *
- * Authors: 
- *
- *          Copyright 2025 
- */
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -28,184 +14,64 @@
 #include <86box/timer.h>
 #include <86box/video.h>
 #include <86box/vid_svga.h>
-#include <86box/vid_rendition_verite.h>
 
 #define ROM_SCREAMIN3D "roms/video/v1000/can76.rom"
 
+typedef struct verite_t {
+    svga_t        svga;
+    rom_t         bios_rom;
+    mem_mapping_t linear_mapping;
+    uint8_t       pci_regs[256];
+    uint8_t       pci_slot;
+    uint8_t       int_line;
+    uint32_t      linear_base;
+} verite_t;
+
 static video_timings_t timing_verite = {
     .type = VIDEO_PCI,
-    .write_b = 2,
-    .write_w = 2,
-    .write_l = 4,
-    .read_b = 20,
-    .read_w = 20,
-    .read_l = 32
+    .write_b = 4,
+    .write_w = 8,
+    .write_l = 16,
+    .read_b = 4,
+    .read_w = 8,
+    .read_l = 16
 };
 
-static void verite_recalctimings(svga_t *svga);
 static void verite_out(uint16_t addr, uint8_t val, void *priv);
 static uint8_t verite_in(uint16_t addr, void *priv);
-static void verite_updatemapping(verite_t *dev);
-static void verite_update_rom_mapping(verite_t *dev);
-
-static void
-verite_update_rom_mapping(verite_t *dev)
-{
-    if (dev->pci_regs[0x30] & 0x01) {
-        uint32_t biosaddr = (dev->pci_regs[0x32] << 16) | (dev->pci_regs[0x33] << 24);
-        mem_mapping_set_addr(&dev->bios_rom.mapping, biosaddr, 0x8000);
-    } else {
-        mem_mapping_disable(&dev->bios_rom.mapping);
-    }
-}
-
-static void
-verite_io_set(verite_t *dev)
-{
-    io_removehandler(0x03c0, 0x0020, verite_in, NULL, NULL, verite_out, NULL, NULL, dev);
-    io_sethandler(0x03c0, 0x0020, verite_in, NULL, NULL, verite_out, NULL, NULL, dev);
-}
-
-static void
-verite_io_remove(verite_t *dev)
-{
-    io_removehandler(0x03c0, 0x0020, verite_in, NULL, NULL, verite_out, NULL, NULL, dev);
-}
-
-static void
-verite_risc_reset(verite_t *dev)
-{
-    memset(&dev->risc, 0, sizeof(dev->risc));
-    dev->risc.regs[0] = 0;
-    dev->risc.halted = 1;
-}
-
-static void
-verite_risc_init(verite_t *dev)
-{
-    verite_risc_reset(dev);
-    for (int i = 0; i < 32; i++) {
-        dev->risc.regs[i] = 0;
-    }
-    dev->risc.regs[32] = 0;
-    dev->risc.regs[48] = 0xffffffff;
-    dev->risc.regs[49] = 0x00ff00ff;
-    dev->risc.regs[50] = 0xff00ff00;
-    dev->risc.regs[51] = 0x0000ffff;
-    dev->risc.regs[52] = 0xffff0000;
-    dev->risc.regs[53] = 0x000000ff;
-    dev->risc.regs[54] = 0x0000ff00;
-    dev->risc.regs[55] = 0x00ff0000;
-    dev->risc.regs[56] = 0xff000000;
-    dev->risc.regs[57] = 0x00ffff00;
-    dev->risc.regs[58] = 0xff00ff00;
-    dev->risc.regs[59] = 0xffffff00;
-    dev->risc.regs[60] = 0x0000ffff;
-    dev->risc.regs[61] = 0x00ffffff;
-    dev->risc.regs[62] = 0xff00ffff;
-    dev->risc.regs[63] = 0xffff00ff;
-}
+static void verite_recalcmapping(verite_t *dev);
 
 static uint8_t
 verite_pci_read(int func, int addr, int len, void *priv)
 {
     verite_t *dev = (verite_t *) priv;
-    uint8_t   ret = 0;
 
     (void) func;
     (void) len;
 
     switch (addr) {
-        case 0x00:
-            ret = VERITE_VENDOR_ID & 0xff;
-            break;
-        case 0x01:
-            ret = (VERITE_VENDOR_ID >> 8) & 0xff;
-            break;
-        case 0x02:
-            ret = VERITE_DEVICE_ID & 0xff;
-            break;
-        case 0x03:
-            ret = (VERITE_DEVICE_ID >> 8) & 0xff;
-            break;
-        case PCI_REG_COMMAND:
-            ret = dev->pci_regs[PCI_REG_COMMAND];
-            break;
-        case PCI_REG_COMMAND_H:
-            ret = dev->pci_regs[PCI_REG_COMMAND_H];
-            break;
-        case 0x06:
-            ret = dev->pci_regs[0x06];
-            break;
-        case 0x07:
-            ret = dev->pci_regs[0x07];
-            break;
-        case 0x08:
-            ret = 0x00;
-            break;
-        case 0x09:
-            ret = 0x00;
-            break;
-        case 0x0a:
-            ret = 0x00;
-            break;
-        case 0x0b:
-            ret = 0x03;
-            break;
-        case 0x0c:
-            ret = dev->pci_regs[0x0c];
-            break;
-        case 0x0d:
-            ret = dev->pci_regs[0x0d];
-            break;
-        case 0x0e:
-            ret = 0x00;
-            break;
-        case 0x0f:
-            ret = 0x00;
-            break;
-        case 0x10:
-            ret = dev->mem_base & 0xff;
-            break;
-        case 0x11:
-            ret = (dev->mem_base >> 8) & 0xff;
-            break;
-        case 0x12:
-            ret = (dev->mem_base >> 16) & 0xff;
-            break;
-        case 0x13:
-            ret = (dev->mem_base >> 24) & 0xff;
-            break;
-        case 0x14:
-        case 0x15:
-        case 0x16:
-        case 0x17:
-            ret = 0x00;
-            break;
-        case 0x30:
-            ret = dev->pci_regs[0x30] & 0x01;
-            break;
-        case 0x31:
-            ret = 0x00;
-            break;
-        case 0x32:
-            ret = dev->pci_regs[0x32];
-            break;
-        case 0x33:
-            ret = dev->pci_regs[0x33];
-            break;
-        case 0x3c:
-            ret = dev->int_line;
-            break;
-        case 0x3d:
-            ret = PCI_INTA;
-            break;
-        default:
-            ret = dev->pci_regs[addr];
-            break;
+        case 0x00: return 0x63;
+        case 0x01: return 0x11;
+        case 0x02: return 0x01;
+        case 0x03: return 0x00;
+        case PCI_REG_COMMAND: return dev->pci_regs[PCI_REG_COMMAND];
+        case 0x07: return 0x02;
+        case 0x08: return 0x00;
+        case 0x09: return 0x00;
+        case 0x0a: return 0x00;
+        case 0x0b: return 0x03;
+        case 0x10: return 0x00;
+        case 0x11: return 0x00;
+        case 0x12: return dev->linear_base >> 16;
+        case 0x13: return dev->linear_base >> 24;
+        case 0x30: return dev->pci_regs[0x30] & 0x01;
+        case 0x31: return 0x00;
+        case 0x32: return dev->pci_regs[0x32];
+        case 0x33: return dev->pci_regs[0x33];
+        case 0x3c: return dev->int_line;
+        case 0x3d: return PCI_INTA;
+        default: return 0;
     }
-
-    return ret;
 }
 
 static void
@@ -218,44 +84,27 @@ verite_pci_write(int func, int addr, int len, uint8_t val, void *priv)
 
     switch (addr) {
         case PCI_REG_COMMAND:
-            dev->pci_regs[PCI_REG_COMMAND] = val & 0x27;
-            verite_updatemapping(dev);
+            dev->pci_regs[PCI_REG_COMMAND] = val & 0x23;
+            verite_recalcmapping(dev);
             break;
-        case PCI_REG_COMMAND_H:
-            dev->pci_regs[PCI_REG_COMMAND_H] = val & 0x03;
-            break;
-        case 0x06:
-            dev->pci_regs[0x06] = val & 0xf0;
-            break;
-        case 0x07:
-            dev->pci_regs[0x07] = val & 0x01;
-            break;
-        case 0x0c:
-            dev->pci_regs[0x0c] = val;
-            break;
-        case 0x0d:
-            dev->pci_regs[0x0d] = val & 0xff;
-            break;
-        case 0x10:
-        case 0x11:
         case 0x12:
+            dev->linear_base = (dev->linear_base & 0xff000000) | ((val & 0xc0) << 16);
+            verite_recalcmapping(dev);
+            break;
         case 0x13:
-            if (addr == 0x10)
-                dev->mem_base = (dev->mem_base & 0xffffff00) | (val & 0x00);
-            else if (addr == 0x11)
-                dev->mem_base = (dev->mem_base & 0xffff00ff) | ((val & 0x00) << 8);
-            else if (addr == 0x12)
-                dev->mem_base = (dev->mem_base & 0xff00ffff) | ((val & 0xc0) << 16);
-            else if (addr == 0x13)
-                dev->mem_base = (dev->mem_base & 0x00ffffff) | (val << 24);
-            dev->mem_base &= 0xffc00000;
-            verite_updatemapping(dev);
+            dev->linear_base = (dev->linear_base & 0x00c00000) | (val << 24);
+            verite_recalcmapping(dev);
             break;
         case 0x30:
         case 0x32:
         case 0x33:
             dev->pci_regs[addr] = val;
-            verite_update_rom_mapping(dev);
+            if (dev->pci_regs[0x30] & 0x01) {
+                uint32_t bios_addr = (dev->pci_regs[0x32] << 16) | (dev->pci_regs[0x33] << 24);
+                mem_mapping_set_addr(&dev->bios_rom.mapping, bios_addr, 0x8000);
+            } else {
+                mem_mapping_disable(&dev->bios_rom.mapping);
+            }
             break;
         case 0x3c:
             dev->int_line = val;
@@ -266,20 +115,14 @@ verite_pci_write(int func, int addr, int len, uint8_t val, void *priv)
 }
 
 static void
-verite_updatemapping(verite_t *dev)
+verite_recalcmapping(verite_t *dev)
 {
     mem_mapping_disable(&dev->linear_mapping);
 
     if (dev->pci_regs[PCI_REG_COMMAND] & PCI_COMMAND_MEM) {
-        if (dev->mem_base) {
-            mem_mapping_set_addr(&dev->linear_mapping, dev->mem_base, VERITE_VRAM_SIZE);
+        if (dev->linear_base) {
+            mem_mapping_set_addr(&dev->linear_mapping, dev->linear_base, 4 << 20);
         }
-    }
-
-    if (dev->pci_regs[PCI_REG_COMMAND] & PCI_COMMAND_IO) {
-        verite_io_set(dev);
-    } else {
-        verite_io_remove(dev);
     }
 }
 
@@ -287,7 +130,6 @@ static void
 verite_out(uint16_t addr, uint8_t val, void *priv)
 {
     verite_t *dev = (verite_t *) priv;
-
     svga_out(addr, val, &dev->svga);
 }
 
@@ -295,7 +137,6 @@ static uint8_t
 verite_in(uint16_t addr, void *priv)
 {
     verite_t *dev = (verite_t *) priv;
-
     return svga_in(addr, &dev->svga);
 }
 
@@ -310,42 +151,24 @@ verite_init(const device_t *info)
     verite_t *dev = malloc(sizeof(verite_t));
     memset(dev, 0, sizeof(verite_t));
 
-    dev->vram_size = VERITE_VRAM_SIZE;
-    dev->vram_mask = dev->vram_size - 1;
-
     rom_init(&dev->bios_rom, ROM_SCREAMIN3D, 0xc0000, 0x8000, 0x7fff, 0, MEM_MAPPING_EXTERNAL);
     mem_mapping_disable(&dev->bios_rom.mapping);
 
-    svga_init(info, &dev->svga, dev, dev->vram_size,
-              verite_recalctimings, verite_in, verite_out,
-              NULL, NULL);
+    video_inform(VIDEO_FLAG_TYPE_SPECIAL, &timing_verite);
 
-    dev->svga.decode_mask = dev->vram_mask;
-    dev->svga.vram_mask = dev->vram_mask;
-    dev->svga.vram_max = dev->vram_size;
-    dev->svga.miscout = 1;
-    dev->svga.bpp = 8;
-    dev->svga.force_old_addr = 1;
+    svga_init(info, &dev->svga, dev, 4 << 20, verite_recalctimings, verite_in, verite_out, NULL, NULL);
 
-    mem_mapping_add(&dev->linear_mapping, 0, 0,
-                    svga_read_linear, svga_readw_linear, svga_readl_linear,
-                    svga_write_linear, svga_writew_linear, svga_writel_linear,
-                    NULL, MEM_MAPPING_EXTERNAL, &dev->svga);
+    mem_mapping_add(&dev->linear_mapping, 0, 0, svga_read_linear, svga_readw_linear, svga_readl_linear, svga_write_linear, svga_writew_linear, svga_writel_linear, NULL, MEM_MAPPING_EXTERNAL, &dev->svga);
     mem_mapping_disable(&dev->linear_mapping);
 
-    dev->pci_regs[PCI_REG_COMMAND] = PCI_COMMAND_IO | PCI_COMMAND_MEM;
-
-    dev->pci_regs[0x30] = 0x00;
-    dev->pci_regs[0x32] = 0x0c;
-    dev->pci_regs[0x33] = 0x00;
+    io_sethandler(0x03c0, 0x0020, verite_in, NULL, NULL, verite_out, NULL, NULL, dev);
 
     pci_add_card(PCI_ADD_NORMAL, verite_pci_read, verite_pci_write, dev, &dev->pci_slot);
 
-    verite_io_set(dev);
-
-    verite_risc_init(dev);
-
-    video_inform(VIDEO_FLAG_TYPE_SPECIAL, &timing_verite);
+    dev->pci_regs[PCI_REG_COMMAND] = 0x83;
+    dev->pci_regs[0x30] = 0x00;
+    dev->pci_regs[0x32] = 0x0c;
+    dev->pci_regs[0x33] = 0x00;
 
     return dev;
 }
@@ -354,9 +177,7 @@ static void
 verite_close(void *priv)
 {
     verite_t *dev = (verite_t *) priv;
-
     svga_close(&dev->svga);
-
     free(dev);
 }
 
@@ -364,7 +185,6 @@ static void
 verite_speed_changed(void *priv)
 {
     verite_t *dev = (verite_t *) priv;
-
     svga_recalctimings(&dev->svga);
 }
 
@@ -372,7 +192,6 @@ static void
 verite_force_redraw(void *priv)
 {
     verite_t *dev = (verite_t *) priv;
-
     dev->svga.fullchange = changeframecount;
 }
 
